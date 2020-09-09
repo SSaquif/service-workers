42510f76da42e488bf28ee8b1bc6d1c41053b094
Sadnan Saquif <ssaquif@gmail.com>
2020-09-08 20:29:21 -0400

    Basic Web Worker Initialised 1. Connection between client and worker has been established 2. Readme Updated

--- web-worker-exercise/web/js/home.js
+++ web-worker-exercise/web/js/home.js
@@ -1,49 +1,62 @@
-(function Home(){
-	"use strict";
+(function Home() {
+  "use strict";
 
-	var startStopBtn;
-	var fibsList;
+  let startStopBtn;
+  let fibsList;
+  let worker;
+  console.log(self);
 
-	document.addEventListener("DOMContentLoaded",ready,false);
+  document.addEventListener("DOMContentLoaded", ready, false);
 
+  // **********************************
 
-	// **********************************
+  function ready() {
+    startStopBtn = document.getElementById("start-stop-btn");
+    fibsList = document.getElementById("fibs");
 
-	function ready() {
-		startStopBtn = document.getElementById("start-stop-btn");
-		fibsList = document.getElementById("fibs");
+    startStopBtn.addEventListener("click", startFibs, false);
+  }
 
-		startStopBtn.addEventListener("click",startFibs,false);
-	}
+  function renderFib(num, fib) {
+    var p = document.createElement("div");
+    p.innerText = `Fib(${num}): ${fib}`;
+    if (fibsList.childNodes.length > 0) {
+      fibsList.insertBefore(p, fibsList.childNodes[0]);
+    } else {
+      fibsList.appendChild(p);
+    }
+  }
 
-	function renderFib(num,fib) {
-		var p = document.createElement("div");
-		p.innerText = `Fib(${num}): ${fib}`;
-		if (fibsList.childNodes.length > 0) {
-			fibsList.insertBefore(p,fibsList.childNodes[0]);
-		}
-		else {
-			fibsList.appendChild(p);
-		}
-	}
+  // Worker related functions
+  function startFibs() {
+    startStopBtn.removeEventListener("click", startFibs, false);
+    startStopBtn.addEventListener("click", stopFibs, false);
 
-	function startFibs() {
-		startStopBtn.removeEventListener("click",startFibs,false);
-		startStopBtn.addEventListener("click",stopFibs,false);
+    startStopBtn.innerText = "Stop";
+    fibsList.innerHTML = "";
 
-		startStopBtn.innerText = "Stop";
-		fibsList.innerHTML = "";
+    // Create new Worker instance
+    // Argument = ulr to a JS file, which will be run in a separete thread
+    worker = new Worker("/js/worker.js");
 
-		// TODO
-	}
+    // We can add MessageEvent Listeners to workers
+    worker.addEventListener("message", onMessage);
+  }
 
-	function stopFibs() {
-		startStopBtn.removeEventListener("click",stopFibs,false);
-		startStopBtn.addEventListener("click",startFibs,false);
+  function stopFibs() {
+    startStopBtn.removeEventListener("click", stopFibs, false);
+    startStopBtn.addEventListener("click", startFibs, false);
 
-		startStopBtn.innerText = "Start";
+    startStopBtn.innerText = "Start";
 
-		// TODO
-	}
+    // TODO
+  }
 
+  function onMessage(event) {
+    // Msg Received from Web Worker
+    console.log("Data Received From Worker", event.data);
+
+    // Send Msg to Worker (to worker's inner scope TBMS)
+    worker.postMessage("Hello from client");
+  }
 })();
--- web-worker-exercise/web/js/worker.js
+++ web-worker-exercise/web/js/worker.js
@@ -2,13 +2,24 @@
 
 var curFib = 0;
 
-// TODO
+// self generally refers to window.self on the browser
+// But since Web Worker runs away from the main thread
+// Here self refers to a DedicatedWorkerGlobalScope Object ()
+console.log("self in worker", self);
 
-// **********************************
+//send to client
+self.postMessage("Hello from web worker");
 
+const onMessage = (event) => {
+  console.log("Client Event:", event);
+};
+
+self.onmessage = onMessage;
+
+// **************Fibonacci*****************
 function fib(n) {
-	if (n < 2) {
-		return n;
-	}
-	return fib(n-1) + fib(n-2);
+  if (n < 2) {
+    return n;
+  }
+  return fib(n - 1) + fib(n - 2);
 }
